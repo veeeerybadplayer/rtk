3628c42e5f6650b837d8f3cc8950bb319dc53331
veeeerybadplayer <isthereonefreename@mail.ru>
2026-03-31 20:40:33 +0300

    a bit of bullshit

--- back/main.py
+++ back/main.py
@@ -1,3 +1,7 @@
 from fastapi import FastAPI
+from rtk.back.users import router as RUser
+from rtk.back.qrgen import router as Rqr
 
 app = FastAPI()
+app.include_router(RUser)
+app.include_router(Rqr)
