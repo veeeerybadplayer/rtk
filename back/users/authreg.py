ac3398c30126c1adf45928edf3c0dca881ca90b8
veeeerybadplayer <isthereonefreename@mail.ru>
2026-06-20 15:04:06 +0300

    added tokens

--- back/users/authreg.py
+++ back/users/authreg.py
@@ -1,4 +1,65 @@
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 from jose import JWTError, jwt
-from passlib.context import CryptContext
+from pwdlib import PasswordHash
+from pwdlib.hashers.argon2 import Argon2Hasher
 from rtk.back.config import settings
+from pydantic import EmailStr
+from rtk.back.users import uservices
+
+pwd_h = PasswordHash((Argon2Hasher()),)
+a_token_lifetime = 30
+r_token_lifetime = 7
+
+def get_pwd_hash(pwd: str) -> str:
+    return pwd_h.hash(pwd)
+
+def verify_pwd(pwd: str, h_pwd: str) -> bool:
+    try:
+        return pwd_h.verify(pwd, h_pwd)
+    except Exception:
+        return False
+    
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(minutes=a_token_lifetime)
+    to_encode.update({"exp": expire, "type": "access"})
+    return jwt.encode(to_encode, settings.crypt_key, settings.crypt_algo)
+
+
+def create_refresh_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + timedelta(days=r_token_lifetime)
+    to_encode.update({"exp": expire, "type": "refresh"})
+    return jwt.encode(to_encode, settings.crypt_key, settings.crypt_algo)
+
+
+def create_tokens_pair(user_id: str) -> dict[str, str]:
+    token_data = {"sub": str(user_id)}
+    return {
+        "access_token": create_access_token(token_data),
+        "refresh_token": create_refresh_token(token_data),
+    }
+
+
+async def authenticate_user(email: EmailStr, password: str):
+    user = await uservices.find_one_or_none(email=email)
+    if not (user and verify_pwd(password, user.hashed_password)):
+        raise Exception
+    return user
+
+
+def refresh_access_token(refresh_token: str) -> str:
+    try:
+        payload = jwt.decode(
+            refresh_token, settings.crypt_key, algorithms=[settings.crypt_algo]
+        )
+        if payload.get("type") != "refresh":
+            raise JWTError("Неправильный тип токена")
+            
+        user_id = payload.get("sub")
+        if user_id is None:
+            raise JWTError("Неверная подпись токена")
+            
+        return create_access_token(data={"sub": user_id})
+    except JWTError as e:
+        raise e
--- back/users/router.py
+++ back/users/router.py
@@ -1,12 +1,14 @@
 from fastapi import APIRouter, HTTPException
-from rtk.back.users.uservices
+from rtk.back.users import uservices
+from rtk.back.users.schemas import SUser
+from rtk.back.users import authreg
 
-router = APIRouter(tags="/users")
+router = APIRouter(tags=["/users"])
 
 @router.post("/register")
-async def reg_user(user_data: SUserAuthReg):
+async def reg_user(user_data: SUser):
     existing_user = await uservices.find_one_or_none(email = user_data.email)
     if existing_user:
         raise HTTPException(status_code=409, detail="Юзер уже есть")
-    hashed_password = get_password_hash(user_data.password)
-    await uservices.add(email=user_data.email, hashed_password=hashed_password)
+    hashed_password = authreg.get_pwd_hash(user_data.pwd)
+    await uservices.add(fio=user_data.fio, rank=user_data.rank, email=user_data.email, hashed_password=hashed_password)
